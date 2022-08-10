11e85c3c4bff16d9774cc2c562a6f51dc46ab947
Jessy11-a <jescamsule11@gmail.com>
2022-08-09 20:36:22 -0700

    improvving page look'

--- lib/Pages/product.dart
+++ lib/Pages/product.dart
@@ -4,18 +4,24 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product etails'),
-        ),
-        body: Column(
-          children: [
-            Text('Details'),
-            ElevatedButton(
-              child: Text('Back'),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        )
-        );
+      appBar: AppBar(
+        title: Text('Product details'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details'),),
+          Container (
+            padding: EdgeInsets.all(10.0),
+            child:ElevatedButton(
+            child: Text('Back'),
+            onPressed: () => Navigator.pop(context),
+          ),)
+        ],
+      ),
+    ); 
   }
 }
