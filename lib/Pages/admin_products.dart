9f6021a3d136894f85cc8b2318830c67531e6e15
Jessy11-a <jescamsule11@gmail.com>
2022-08-11 07:05:32 -0700

    adding tabs

--- lib/Pages/admin_products.dart
+++ lib/Pages/admin_products.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
-
 import './products.dart';
+import './product_create.dart';
+import './product_list.dart';
 
 class AdminProducts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: Drawer(
-            child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
-            ),
-            ListTile(
-              title: Text('All products'),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductsPage()));
-              },
-            ),
-          ],
-        )),
-        appBar: AppBar(
-          title: Text('Manage products'),
-        ),
-        body: Center(child: Text("Manage your products")));
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: Drawer(
+              child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All products'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => ProductsPage()));
+                },
+              ),
+            ],
+          )),
+          appBar: AppBar(
+              title: Text('Manage products'),
+              bottom: TabBar(tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: 'Create Product',
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: 'My products')
+              ])),
+          body: TabBarView(children: [
+            ProductCreatePage(),
+            ProductListPage(),
+            ],),
+    ),);
   }
 }
